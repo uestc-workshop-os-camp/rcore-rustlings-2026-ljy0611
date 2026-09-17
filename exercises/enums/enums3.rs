87e0f9e41803c050bc24b609a246a1ba082f9c89
ljy0611 <3404835296@qq.com>
2026-09-17 17:51:54 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -48,6 +48,7 @@ impl State {
             Message::Echo(s) => self.echo(s),
             Message::Move(p) => self.move_position(p),
             Message::Quit => self.quit(),
+         }
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
