e43cde89d2cd455fe1ed9d11524caaacca45442b
ljy0611 <3404835296@qq.com>
2026-09-22 19:18:53 +0800

    update

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -10,7 +10,7 @@
 // hint.
 
 
-pub fn generate_nametag_text(name: String) -> Result<String> {
+pub fn generate_nametag_text(name: String) -> Result<String,String> {
     if name.is_empty() {
         // Empty names aren't allowed.
         Err("`name` was empty; it must be nonempty.".to_string())
